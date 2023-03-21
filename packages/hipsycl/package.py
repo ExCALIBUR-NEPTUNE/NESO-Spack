c76240b3145bc624301680ceda9876359ce00670
Will Saunders <will.saunders@ukaea.uk>
2023-03-21 14:24:53 +0000

    reordered hipsycl versions to be descending

--- packages/hipsycl/package.py
+++ packages/hipsycl/package.py
@@ -36,19 +36,17 @@ class Hipsycl(CMakePackage):
 
     version("stable", branch="stable", submodules=True)
     version(
-        "0.9.2",
-        commit="49fd02499841ae884c61c738610e58c27ab51fdb",
+        "0.9.4",
+        commit="99d9e24d462b35e815e0e59c1b611936c70464ae",
         submodules=True)
     version(
         "0.9.3",
         commit="51507bad524c33afe8b124804091b10fa25618dc",
         submodules=True)
     version(
-        "0.9.4",
-        commit="99d9e24d462b35e815e0e59c1b611936c70464ae",
-        submodules=True,
-        preferred=True,
-    )
+        "0.9.2",
+        commit="49fd02499841ae884c61c738610e58c27ab51fdb",
+        submodules=True)
 
     variant(
         "cuda",
